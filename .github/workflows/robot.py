2f05cbb2f684a7a9dfe34bc358196c69644f0f00
YukoSky <daffetyxd@gmail.com>
2020-07-26 10:45:31 -0300

    (BUILD) MIUI

--- .github/workflows/robot.py
+++ .github/workflows/robot.py
@@ -15,21 +15,19 @@ dab = os.getenv('DOWNLOAD_AB')
 
 with pyrogram.Client('bot', os.getenv('API_ID'), os.getenv('API_HASH'), bot_token=os.getenv('TOKEN')) as client:
     client.send_message(
-        text=f"""- <b>{rom} GSI For A-Only • A/B Devices</b>
+        text=f"""<b>{rom} GSI For A/AB Devices</b>
 
-<b>- Firmware Base:</b> <a href="{romlink}">HERE</a>
+<b>Firmware Base:</b> <a href="{romlink}">HERE</a>
 
-<b>- Information:</b>
-
+<b>Information:</b>
 <code>{cat}</code>
 
-<b>- Download A-Only:</b> <a href="{da}">HERE</a>
-<b>- Download A/B:</b> <a href="{dab}">HERE</a>
+<b>Download A-Only:</b> <a href="{da}">HERE</a>
+<b>Download A/B:</b> <a href="{dab}">HERE</a>
 
-<b>- Thanks to:</b> <a href="https://github.com/yukosky/ErfanGSIs/graphs/contributors">Contributors List</a>
+<b>Thanks to:</b> <a href="https://github.com/yukosky/ErfanGSIs/graphs/contributors">Contributors List</a>
 
-<b>• YuMi Projekt </b> - Channel: @yuprojekt
-<b>• YuMi Projekt </b> - Group: @yusupport
+<b>Treble Experience </b> - Channel: @TrebleExperience
 
 • <b>Ported using ErfanGSIs Tool - <a href="https://github.com/yukosky/ErfanGSIs">Yuko's Based</a>""",
         chat_id=os.getenv('CHAT_ID'),
